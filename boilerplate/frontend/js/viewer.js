7506280573e6da6d0e43ba755ab053b6c82c4b8b
meastoso <meastoso@gmail.com>
2017-09-25 18:17:05 -0400

    Initial check-in for view page work and menu toggling

--- boilerplate/frontend/js/viewer.js
+++ boilerplate/frontend/js/viewer.js
@@ -18,10 +18,21 @@ or in the "license" file accompanying this file. This file is distributed on an 
 //NOTE: NORMALLY I THINK THIS IS GARBAGE BUT I WANT TO FINISH THIS PROJECT
 let CHANNEL_ID = 0;
 
-const app = angular.module("viewerApp",[]);
+const app = angular.module("viewerApp",['angularjs-gauge', 'ngMaterial']);
 app.controller("ViewerController", function($scope, $http, $compile) {
 	// initialize current configuration
 	console.log("loading authorized");
+	
+	$scope.extensionOpen = false; // used to toggle extension view
+	$scope.toggleShow = function() {
+		$scope.extensionOpen = !$scope.extensionOpen;
+	}
+	
+	$scope.showBody = true; // used to toggle body content
+	$scope.clickJob = function(jobStr, charAndServerStr) {
+		// TODO: implement
+		$scope.showBody = true;
+	}
 	
 	window.Twitch.ext.onAuthorized(function(auth) {
 		var parts = auth.token.split(".");
@@ -59,5 +70,58 @@ app.controller("ViewerController", function($scope, $http, $compile) {
 			//$("#gettingCharDataLoading").hide();
 		});
 		console.log("viewer controller loaded ok!");
+	});
+	
+	// TODO: DELETE THIS AFTER TESTING WITHOUT THE STREAM ON
+	CHANNEL_ID = '51673052'; // hardcoded twitch.tv/meastoso
+	
+	// first get all the characters configured by this channel
+	const getCharsURL = '/getChars';
+	$http({
+		url : getCharsURL,
+		method : "GET",
+		params : {
+			channelID: CHANNEL_ID
+		}
+		}).then(function successCallback(response) {
+			console.log('initialization of getChars success');
+			console.log(response);
+			const charArr = response.data;
+			// build character doms here
+		
+		}, function errorCallback(response) {
+			console.log('initialization of getChars failed!');
+			console.log(response);
+		});
+	
+	
+	
+	// for each character, go get the summaries and populate here
+	const getSummaryForCharURL = '/getSummaryForChar';
+	$http({
+		url : getSummaryForCharURL,
+		method : "GET",
+		params : {
+			charName: 'Meastoso Disperde',
+			serverName: 'Behemoth',
+			realmName: 'NA'
+		}
+	}).then(function successCallback(charSummary) {
+		console.log('initialization of getSummaries success');
+		if (Object.prototype.toString.call( charSummary ) === '[object Array]' ) {
+			// TODO: draw up this character's summary here
+			
+		}
+		else {
+			// something went wrong fetching data from fflogs, maybe 
+			// wrong character info or the character doesn't exist in fflogs
+			// TODO: show error message in view here
+		}
+		console.log(charSummary);
+	
+	}, function errorCallback(response) {
+		console.log('initialization of getConfig failed!');
+		console.log(response);
+		//$("#gettingCharDataLoading").hide();
 	});
 });
